88881b2f80e58c3240dfec97bebb546ba7ac8b68
haokaili <>
2021-06-08 17:58:34 +0000

    splash, title and menu copy 1

--- haokai/js/game.js
+++ haokai/js/game.js
@@ -9,13 +9,11 @@
 import SplashScene from './splashScene.js'
 import TitleScene from './titleScene.js'
 import MenuScene from './menuScene.js'
-import GameScene from './gameScene.js'
 
 // Our game scene
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
 const menuScene = new MenuScene()
-const gameScene = new GameScene()
 
 //* Game scene */
 const config = {
@@ -44,7 +42,6 @@ const game = new Phaser.Game(config)
 game.scene.add('splashScene', splashScene)
 game.scene.add('titleScene', titleScene)
 game.scene.add('menuScene', menuScene)
-game.scene.add('gameScene', gameScene)
 
 // start title
 game.scene.start('splashScene')
