3055c46019db3d577b844dc92da278fb4511b5cf
daniel-pawelko-ics20 <82897918+daniel-pawelko-ics20@users.noreply.github.com>
2021-06-09 09:53:40 -0700

    Update main.py
    basic concept working(blocks keyboard when detects intrusions)...does not unblock it yet

--- pawelko/main.py
+++ pawelko/main.py
@@ -1,1 +1,32 @@
-print("hello, world")
+import pyWinhook
+
+# Getting configuration
+with open("config.json", "r") as f:
+    config = json.load(f)
+preset = config["preset"]
+history = config[preset]["history"]
+threshold = config[preset]["threshold"]
+
+# defining variables
+last_key = time.time()
+history_array = []
+
+# Gets called for each keystroke
+def KeyDown(event):
+    global history_array, history, threshold, last_key
+    
+    # getting time diff
+    time_diff = -1 * (last_key - time.time())
+    history_array.append(time_diff)
+    if len(history_array) > history:
+        history_array.pop(0)
+        time_total = sum(history_array) / history
+        if time_total < threshold:
+              last_key = time.time()
+              return False
+            
+            
+hm = pyWinhook.HookManager()
+hm.KeyDown = KeyDown
+hm.HookKeyboard()
+pythoncom.PumpMessages()
