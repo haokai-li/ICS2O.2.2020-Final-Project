ee72db4b3096ebf26df00a5568c6664efd4ef5bc
RyanShaw21 <82897945+Ryan-Shaw-2@users.noreply.github.com>
2021-06-09 17:02:44 +0000

    Added game.js

---
+++ shaw/js/game.js
@@ -0,0 +1,50 @@
+/* global Phaser */
+
+// Copyright (c) 2021 Ryan-Shaw-2 All rights reserved
+//
+// Created by: Ryan-Shaw-2
+// Created on: Apr 2021
+// This is the Phaser3 configuration file
+
+import SplashScene from './splashScene.js'
+import TitleScene from './titleScene.js'
+import MenuScene from './menuScene.js'
+import GameScene from './gameScene.js'
+
+// Our game Scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+//* Game Scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+
+// load scenes
+// NOTE: remember any "key" is global and CAN NOT be reused
+game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
+game.scene.add('gameScene', gameScene)
+
+// start title
+game.scene.start('splashScene')
