94751e521be6a589743da522e7667476d7b3e5c4
RyanShaw21 <82897945+Ryan-Shaw-2@users.noreply.github.com>
2021-06-10 19:07:38 +0000

    Added Game Grid

--- shaw/js/gameScene.js
+++ shaw/js/gameScene.js
@@ -7,6 +7,20 @@
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
+  createGameGrid (x, y) {
+    while (y < 1080){
+      while (x < 1920){
+        const cell = this.add.rectangle(x, y, 192, 180)
+        cell.setStrokeStyle(2, 0x000000)
+        this.gameGridCellGroup.add(cell)
+        x += 192
+      }
+      if (x > 1920) {
+        x = 96
+        y += 180
+      }
+    }
+  }
   constructor () {
     super({ key: 'gameScene' })
 
@@ -25,8 +39,13 @@ class GameScene extends Phaser.Scene {
   }
 
   create (data) {
+    // background
     this.background = this.add.image(0, 0, 'gameSceneBackground')
     this.background.setOrigin(0, 0)
+
+    // Game Grid
+    this.gameGridCellGroup = this.add.group()
+    this.createGameGrid(96, 270)
   }
 
   update (time, delta) {
