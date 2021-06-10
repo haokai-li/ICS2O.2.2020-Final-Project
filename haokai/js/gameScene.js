2c3e21d67f7bd7232edb06c6ba50ee58db32381a
haokaili <82911890+haokai-li@users.noreply.github.com>
2021-06-10 17:39:01 +0000

    find problem 1 6.10

--- haokai/js/gameScene.js
+++ haokai/js/gameScene.js
@@ -24,6 +24,7 @@ class GameScene extends Phaser.Scene {
     this.background = null
     this.ship = null
     this.fireMissle = false
+    this.time = 0
     this.score = 0
     this.scoreText = null
     this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
@@ -52,7 +53,12 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle) {
+      while (time == 0) {
+        thread.sleep(1 * 1000)
+        this.score = this.score + 1
+      }
+    }
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
