b5cdd26b5123374447715f410f54ba82fe9fa614
RyanShaw21 <82897945+Ryan-Shaw-2@users.noreply.github.com>
2021-06-11 18:50:45 +0000

    Changed to only show cell under pointer

--- shaw/js/gameScene.js
+++ shaw/js/gameScene.js
@@ -10,8 +10,11 @@ class GameScene extends Phaser.Scene {
   createGameGrid (x, y) {
     while (y < 1080){
       while (x < 1920){
-        const cell = this.add.rectangle(x, y, 192, 180)
+        const cell = this.add.rectangle(x, y, 192, 180).setInteractive()
         cell.setStrokeStyle(2, 0x000000)
+        cell.alpha = 0.01
+        cell.cellClicked = false
+        cell.defenderPlaced = false
         this.gameGridCellGroup.add(cell)
         x += 192
       }
@@ -21,6 +24,10 @@ class GameScene extends Phaser.Scene {
       }
     }
   }
+  createDefender () {
+    console.log('test')
+  }
+
   constructor () {
     super({ key: 'gameScene' })
 
@@ -36,6 +43,7 @@ class GameScene extends Phaser.Scene {
 
     // images
     this.load.image('gameSceneBackground', 'assets/gameSceneBackground.png')
+    this.load.image('defender', 'assets/spaceMarine.png')
   }
 
   create (data) {
@@ -43,12 +51,37 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'gameSceneBackground')
     this.background.setOrigin(0, 0)
 
-    // Game Grid
+    // Game Grid cell group
     this.gameGridCellGroup = this.add.group()
     this.createGameGrid(96, 270)
+
+    // Defender group
+    this.defenderGroup = this.add.group()
   }
 
   update (time, delta) {
+    // only shows the cell that the pointer is currently over
+    this.gameGridCellGroup.children.each(function(cell) {
+      cell.on('pointerover', function(pointer, x, y) {
+        cell.alpha = 1
+      })
+      cell.on('pointerout', function() {
+        cell.alpha = 0.01
+      })
+    })
+
+    // Checks if a cell has been clicked by the pointer
+    this.gameGridCellGroup.children.each(function(cell) {
+      cell.on('pointerdown', function(x, y) {
+        if (this.cellClicked === false) {
+          this.defenderPlaced = true
+          this.cellClicked = true
+          console.log(cell.x, cell.y)
+      })
+      cell.on('pointerup', function() {
+        this.cellClicked = false
+      })
+    })
   }
 }
 
